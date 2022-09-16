b88bd6c65f10ca8995e3ee645a9616add0dd5dc4
Rajeshwar Patlolla <rajeshwar.patlolla@gmail.com>
2022-09-16 15:18:55 +0530

    fixed lint issues in index.js files

--- index.js
+++ index.js
@@ -1,33 +1,36 @@
-'use strict';
-
 const Hapi = require('@hapi/hapi');
 const Inert = require('@hapi/inert');
 const Vision = require('@hapi/vision');
+const HapuGood = require('@hapi/good');
 const HapiSwagger = require('hapi-swagger');
-const mongoose = require('mongoose');
+// const mongoose = require('mongoose');
 
 const Config = require('./config');
 const Routes = require('./routes');
-const Pack = require('./package');
+const Pack = require('./package.json');
 
-mongoose.connect(Config.database.uri, { useNewUrlParser: true, useCreateIndex: true, useUnifiedTopology: true });
+// mongoose.connect(Config.database.uri, {
+//   useNewUrlParser: true,
+//   useCreateIndex: true,
+//   useUnifiedTopology: true,
+// });
 
-let db = mongoose.connection;
+// let db = mongoose.connection;
 
-db.on('connected', function callback() {
-  console.log('Connected to DB.');
-  init();
-});
+// db.on('connected', function callback() {
+//   console.log('Connected to DB.');
+//   init();
+// });
 
-db.on('error', function() {
-  console.log('Connection to DB failed!');
-  process.exit(0);
-});
+// db.on('error', function () {
+//   console.log('Connection to DB failed!');
+//   process.exit(0);
+// });
 
-db.on('disconnected', function(err) {
-  console.log('Connection teminated to DB ', err);
-  process.exit(0);
-});
+// db.on('disconnected', function (err) {
+//   console.log('Connection teminated to DB ', err);
+//   process.exit(0);
+// });
 
 const init = async () => {
   const server = Hapi.server({
@@ -90,7 +93,7 @@ const init = async () => {
       options: swaggerOptions,
     },
     {
-      plugin: require('@hapi/good'),
+      plugin: HapuGood,
       options: goodOptions,
     },
   ]);
@@ -104,14 +107,12 @@ const init = async () => {
   }
   server.route(Routes);
 
-  server.ext('onPostAuth', async (req, h) => {
-    return h.continue;
-  });
+  server.ext('onPostAuth', async (req, h) => h.continue);
 };
 
-process.on('unhandledRejection', err => {
+process.on('unhandledRejection', (err) => {
   console.error(err);
   process.exit(1);
 });
 
-// init();
+init();
